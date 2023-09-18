f598b60f0cc1571bf87d15476bf0c6d09bec23e5
mthaher3@gmail.com <mthaher3@gmail.com>
2023-09-18 04:51:44 +0300

    create edit note view

--- nots_app/notes_app/lib/widgets/custom_note_item.dart
+++ nots_app/notes_app/lib/widgets/custom_note_item.dart
@@ -1,55 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(left: 16, top: 24, bottom: 24),
-      decoration: BoxDecoration(
-        color: const Color(0xFFFFCC80),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: const Text(
-              'Flutter Tips',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 26,
-              ),
-            ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 16, bottom: 16),
-              child: Text(
-                'Bulid you career with mahmoud thhaher',
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(
+          builder: (context) {
+            return const EditNoteView();
+          },
+        ));
+      },
+      child: Container(
+        padding: const EdgeInsets.only(left: 16, top: 24, bottom: 24),
+        decoration: BoxDecoration(
+          color: const Color(0xFFFFCC80),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: const Text(
+                'Flutter Tips',
                 style: TextStyle(
-                  color: Colors.black.withOpacity(0.5),
-                  fontSize: 18,
+                  color: Colors.black,
+                  fontSize: 26,
+                ),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 16, bottom: 16),
+                child: Text(
+                  'Bulid you career with mahmoud thhaher',
+                  style: TextStyle(
+                    color: Colors.black.withOpacity(0.5),
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.delete,
+                  color: Colors.black,
+                  size: 30,
                 ),
               ),
             ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.delete,
-                color: Colors.black,
-                size: 30,
+            Padding(
+              padding: const EdgeInsets.only(right: 24),
+              child: Text(
+                'May12,2020',
+                style: TextStyle(
+                    color: Colors.black.withOpacity(0.5), fontSize: 16),
               ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 24),
-            child: Text(
-              'May12,2020',
-              style:
-                  TextStyle(color: Colors.black.withOpacity(0.5), fontSize: 16),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
