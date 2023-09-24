ca96006f7adb5b909b518536e6643585dc6648c8
mthaher3@gmail.com <mthaher3@gmail.com>
2023-09-25 01:03:57 +0300

    add custom list view item to home

--- bookly/lib/Features/Home/presentation/views/widgets/home_view_body.dart
+++ bookly/lib/Features/Home/presentation/views/widgets/home_view_body.dart
@@ -1,7 +1,6 @@
 import 'package:bookly/Features/Home/presentation/views/widgets/custom_app_bar.dart';
-import 'package:bookly/core/utils/assets.dart';
+import 'package:bookly/Features/Home/presentation/views/widgets/custom_list_view_item.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -11,6 +10,7 @@ class HomeViewBody extends StatelessWidget {
     return const Column(
       children: [
         CustomAppBar(),
+        CustomListViewItem(),
       ],
     );
   }
--- bookly/lib/core/utils/assets.dart
+++ bookly/lib/core/utils/assets.dart
@@ -1,3 +1,4 @@
 class AssetsData {
   static const logo = 'assets/images/Logo.png';
+  static const imageLion = 'assets/images/lion.png';
 }
