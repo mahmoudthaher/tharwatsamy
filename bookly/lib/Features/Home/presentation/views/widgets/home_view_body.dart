8f41535eac8bf6e3a96a433e8bd716e137224099
mthaher3@gmail.com <mthaher3@gmail.com>
2023-09-26 02:09:01 +0300

    best seller list view done(scroll)

--- bookly/lib/Features/Home/presentation/views/widgets/home_view_body.dart
+++ bookly/lib/Features/Home/presentation/views/widgets/home_view_body.dart
@@ -1,6 +1,7 @@
-import 'package:bookly/Features/Home/presentation/views/widgets/best_seller_list_view_item.dart';
+import 'package:bookly/Features/Home/presentation/views/widgets/best_seller_list_view.dart';
 import 'package:bookly/Features/Home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly/Features/Home/presentation/views/widgets/feature_list_view.dart';
+import 'package:bookly/constants.dart';
 import 'package:bookly/core/utils/style.dart';
 import 'package:flutter/material.dart';
 
@@ -10,20 +11,35 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const SafeArea(
-      child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 30),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            CustomAppBar(),
-            SizedBox(height: 8),
-            FeatureBooks(),
-            SizedBox(height: 50),
-            Text('Best Seller', style: Styles.textStyle18),
-            SizedBox(height: 20),
-            BestSellerListViewItem(),
-          ],
-        ),
+      child: CustomScrollView(
+        physics: BouncingScrollPhysics(),
+        slivers: [
+          SliverToBoxAdapter(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: kPaddingHomeView,
+                  child: CustomAppBar(),
+                ),
+                SizedBox(height: 8),
+                FeatureBooks(),
+                SizedBox(height: 50),
+                Padding(
+                  padding: kPaddingHomeView,
+                  child: Text('Best Seller', style: Styles.textStyle18),
+                ),
+                SizedBox(height: 20),
+              ],
+            ),
+          ),
+          SliverFillRemaining(
+            child: Padding(
+              padding: kPaddingHomeView,
+              child: BestSellerListView(),
+            ),
+          ),
+        ],
       ),
     );
   }
--- bookly/lib/constants.dart
+++ bookly/lib/constants.dart
@@ -3,3 +3,4 @@ import 'package:flutter/material.dart';
 const kPrimaryColor = Color(0xFF100B20);
 const kTransitionDuration = Duration(milliseconds: 250);
 const kGtSectraFine = 'GT Sectra Fine';
+const kPaddingHomeView = EdgeInsets.symmetric(horizontal: 30);
