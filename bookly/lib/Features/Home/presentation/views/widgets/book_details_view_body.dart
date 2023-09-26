c060b8785216d9f2158d09bdb9b554d3a2a4a189
mthaher3@gmail.com <mthaher3@gmail.com>
2023-09-26 23:16:47 +0300

    add package size & devise preview

--- bookly/lib/Features/Home/presentation/views/widgets/book_details_view_body.dart
+++ bookly/lib/Features/Home/presentation/views/widgets/book_details_view_body.dart
@@ -1,18 +1,26 @@
 import 'package:bookly/Features/Home/presentation/views/widgets/custom_book_detail_app_bar.dart';
+import 'package:bookly/Features/Home/presentation/views/widgets/custom_list_view_item.dart';
 import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
   const BookDetailsViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return SafeArea(
       child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 30),
+        padding: const EdgeInsets.symmetric(horizontal: 30),
         child: Column(
           children: [
-            SizedBox(height: 20),
-            CustomBookDetailAppBar(),
+            const SizedBox(height: 20),
+            const CustomBookDetailAppBar(),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: 12.h,
+              ),
+              child: const FeaturedListViewItem(),
+            )
           ],
         ),
       ),
--- bookly/lib/main.dart
+++ bookly/lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:bookly/constants.dart';
 import 'package:bookly/core/utils/app_router.dart';
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:sizer/sizer.dart';
 
 void main() {
-  runApp(const BooklyApp());
+  runApp(DevicePreview(
+    enabled: !kReleaseMode,
+    builder: (context) => const BooklyApp(), // Wrap your app
+  ));
 }
 
 class BooklyApp extends StatelessWidget {
@@ -12,15 +18,21 @@ class BooklyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: AppRoute.router,
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: kPrimaryColor,
-        textTheme: GoogleFonts.montserratTextTheme(
-          ThemeData.dark().textTheme,
-        ),
-      ),
+    return Sizer(
+      builder: (context, orientation, deviceType) {
+        return MaterialApp.router(
+          locale: DevicePreview.locale(context),
+          builder: DevicePreview.appBuilder,
+          routerConfig: AppRoute.router,
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData.dark().copyWith(
+            scaffoldBackgroundColor: kPrimaryColor,
+            textTheme: GoogleFonts.montserratTextTheme(
+              ThemeData.dark().textTheme,
+            ),
+          ),
+        );
+      },
     );
   }
 }
