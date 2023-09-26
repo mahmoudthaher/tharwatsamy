e6926be6eeb83e9b7b84aceb5dae800f0009d8cc
mthaher3@gmail.com <mthaher3@gmail.com>
2023-09-27 02:27:31 +0300

    create similar books list view

--- bookly/lib/Features/Home/presentation/views/widgets/book_details_view_body.dart
+++ bookly/lib/Features/Home/presentation/views/widgets/book_details_view_body.dart
@@ -2,6 +2,7 @@ import 'package:bookly/Features/Home/presentation/views/widgets/book_rating.dart
 import 'package:bookly/Features/Home/presentation/views/widgets/books_action.dart';
 import 'package:bookly/Features/Home/presentation/views/widgets/custom_book_detail_app_bar.dart';
 import 'package:bookly/Features/Home/presentation/views/widgets/custom_book_image.dart';
+import 'package:bookly/Features/Home/presentation/views/widgets/similar_books_list_view_.dart';
 import 'package:bookly/core/utils/style.dart';
 import 'package:flutter/material.dart';
 import 'package:sizer/sizer.dart';
@@ -48,7 +49,19 @@ class BookDetailsViewBody extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
             ),
             const SizedBox(height: 37),
-            const BooksAction()
+            const BooksAction(),
+            const SizedBox(height: 50),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'You can also like',
+                style: Styles.textStyle14.copyWith(
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+            const SizedBox(height: 16),
+            const SimilarBooksListView(),
           ],
         ),
       ),
