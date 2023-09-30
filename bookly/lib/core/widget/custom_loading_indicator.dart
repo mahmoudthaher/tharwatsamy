5abb6f50235cf4cf06d860314ffb46d901d0aa39
mthaher3@gmail.com <mthaher3@gmail.com>
2023-09-30 04:15:33 +0300

    install shimmer flutter

--- bookly/lib/core/widget/custom_loading_indicator.dart
+++ bookly/lib/core/widget/custom_loading_indicator.dart
@@ -1,12 +1,42 @@
+import 'package:bookly/core/widget/placeholders.dart';
 import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
 
 class CustomLoadingIndicator extends StatelessWidget {
   const CustomLoadingIndicator({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: CircularProgressIndicator(),
-    );
+    return Shimmer.fromColors(
+        baseColor: Colors.grey.shade300,
+        highlightColor: Colors.grey.shade100,
+        enabled: true,
+        child: const SingleChildScrollView(
+          physics: NeverScrollableScrollPhysics(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              BannerPlaceholder(),
+              TitlePlaceholder(width: double.infinity),
+              SizedBox(height: 16.0),
+              ContentPlaceholder(
+                lineType: ContentLineType.threeLines,
+              ),
+              SizedBox(height: 16.0),
+              TitlePlaceholder(width: 200.0),
+              SizedBox(height: 16.0),
+              ContentPlaceholder(
+                lineType: ContentLineType.twoLines,
+              ),
+              SizedBox(height: 16.0),
+              TitlePlaceholder(width: 200.0),
+              SizedBox(height: 16.0),
+              ContentPlaceholder(
+                lineType: ContentLineType.twoLines,
+              ),
+            ],
+          ),
+        ));
   }
 }
